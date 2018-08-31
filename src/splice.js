92501ec2742efb8ca7df3fe7004d5213d0a444fb
linweiws <1021069119@qq.com>
2018-08-31 20:54:55 +0800

    Add code.
    Signed-off-by: linweiws <1021069119@qq.com>

---
+++ src/splice.js
@@ -0,0 +1,42 @@
+if(!Array.prototype._splice){
+	Array.prototype._splice=(function(){
+		"use strict";
+		var length=function(array){
+			return array.length;
+		};
+		var typeOf=function(value){
+			return typeof value;
+		};
+		var isNumber=function(value){
+			return typeOf(value)==='number'&&isFinite(value);
+		};
+		var remove=function(array,del,start,c){
+			for(var k=0,i;k<c;k+=1){
+				del.push(array[start]);
+				for(i=start;i<length(array)-1;i+=1){
+					array[i]=array[i+1];
+				}
+				array.length-=1;
+			}
+		};
+		var add=function(array,obj,start){
+			for(var k=length(obj)-1,i;k>=2;k-=1){
+				for(i=length(array)-1;i>=start;i-=1){
+					array[i+1]=array[i];
+				}
+				array[start]=obj[k];
+			}
+		};
+		return function(start,count){
+			var del=[];
+			if(isNumber(start)&&isNumber(count)){
+				if(start>=length(this)-1||count>length(this)-start){
+					return;
+				}
+				remove(this,del,start,count);
+				add(this,arguments,start);
+			}
+			return del;
+		};
+	})();
+}

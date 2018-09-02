b032629b3ed092730721c983f6952f2665a472ce
linweiws <1021069119@qq.com>
2018-09-02 19:40:56 +0800

    新增源代码
    Signed-off-by: linweiws <1021069119@qq.com>

---
+++ src/concat.js
@@ -0,0 +1,30 @@
+if(!Array.prototype._concat){
+	Array.prototype._concat=(function(){
+		"use strict";
+		var length=function(obj){
+			return obj.length;
+		};
+		var push=function(array,another){
+			for(var i=0;i<length(array);i+=1){
+				another.push(array[i]);
+			}
+		};
+		var concat=function(array,obj){
+			for(var i=0,j;i<length(obj);i+=1){
+				if(Array.isArray(obj[i])){
+					for(j=0;j<length(obj[i]);j+=1){
+						array.push(obj[i][j]);
+					}
+				} else{
+					array.push(obj[i]);
+				}
+			}
+		};
+		return function(){
+			var array=[],args=arguments;
+			push(this,array);
+			concat(array,args);
+			return array;
+		};
+	})();
+}

093e340a970d2ee2696a85aa75c6afd6bc33e054
linweiws <1021069119@qq.com>
2018-09-17 21:33:24 +0800

    docs: add comment about flattenDepth.js
    Signed-off-by: linweiws <1021069119@qq.com>

--- src/flattenDepth.js
+++ src/flattenDepth.js
@@ -1,18 +1,44 @@
+/**
+ * 2018-09-17
+ * @LinWei
+ *
+ * feature like _.flattenDepth in lodash.
+ * see https://lodash.com/docs/4.17.10#flattenDepth.
+ *
+ * for example:
+ * 
+ * var array = [1, [2, 3, [4]], 5, [6]];
+ *
+ * console.log(array._flattenDepth(1));
+ * // => [1, 2, 3, [4], 5, 6]
+ *
+ * console.log(array._flattenDepth(2));
+ * // => [1, 2, 3, 4, 5, 6]
+ *
+ */
+
 if(!Array.prototype._flattenDepth){
 	Array.prototype._flattenDepth=(function(){
 		"use strict";
+
 		var length=function(array){
 			return array.length;
 		};
+
 		var typeOf=function(value){
 			return typeof value;
 		};
+
+		// check if it is number.
 		var isNumber=function(value){
 			return typeOf(value)==='number'&&isFinite(value);
 		};
+
+		// check if it is array.
 		var isArray=function(obj){
 			return Array.isArray(obj);
 		};
+
 		var flatten=function(array,result,depth,c){
 			for(var i=0;i<length(array);i+=1){
 				if(isArray(array[i])&&c<depth){
@@ -22,6 +48,7 @@ if(!Array.prototype._flattenDepth){
 				}
 			}
 		};
+		
 		return function(depth){
 			depth=isNumber(depth)&&depth>=0?depth:0;
 			var result=[];

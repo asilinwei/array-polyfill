788a4609f09dfde944d9f3a4be64d407552bce19
linweiws <1021069119@qq.com>
2018-09-18 15:02:04 +0800

    docs: add comment about zip.js
    Signed-off-by: linweiws <1021069119@qq.com>

--- src/zip.js
+++ src/zip.js
@@ -1,9 +1,28 @@
+/**
+ * 2018-09-18
+ * @LinWei
+ *
+ * feature like _.zip in lodash.
+ * see https://lodash.com/docs/4.17.10#zip.
+ *
+ * for example:
+ *
+ * var array = [['a', 'b'], [1, 2], [true, false]];
+ *
+ * console.log(array._zip());
+ * // => [['a', 1, true], ['b', 2, false]]
+ *
+ */
+
 if(!Array.prototype._zip){
 	Array.prototype._zip=(function(){
 		"use strict";
+
 		var length=function(array){
 			return array.length;
 		};
+
+		// maximum length
 		var max=function(array){
 			for(var i=0,max=0;i<length(array);i+=1){
 				if(!Array.isArray(array[i])){
@@ -15,6 +34,7 @@ if(!Array.prototype._zip){
 			}
 			return max;
 		};
+
 		var zip=function(array,result,len){
 			for(var i=0,j,chunk;i<len;i+=1){
 				chunk=[];
@@ -27,6 +47,7 @@ if(!Array.prototype._zip){
 				result.push(chunk);
 			}
 		};
+		
 		return function(){
 			var result=[];
 			zip(this,result,max(this));

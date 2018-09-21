32a0d8f4bc47795baaa578afb97a3c3b07e4bf1f
linweiws <1021069119@qq.com>
2018-09-21 17:04:44 +0800

    docs: add comment about fromPairs.js
    Signed-off-by: linweiws <1021069119@qq.com>

--- src/fromPairs.js
+++ src/fromPairs.js
@@ -1,9 +1,29 @@
+/**
+ * 2018-09-21
+ * @LinWei
+ *
+ * feature like _.fromPairs in lodash.
+ * see https://lodash.com/docs/4.17.10#fromPairs.
+ *
+ * for example:
+ *
+ * var array=[['a',1],['b',2]];
+ *
+ * console.log(array._fromPairs());
+ * // => {'a': 1, 'b': 2}
+ *
+ */
+
 if(!Array.prototype._fromPairs){
 	Array.prototype._fromPairs=(function(){
 		"use strict";
+
+		// the length of array-like object.
 		var length=function(obj){
 			return obj.length;
 		};
+
+		// check if it is array-like object.
 		var isArrayLike=function(obj){
 			return typeof obj==='object'&&
 				   obj!==null&&
@@ -11,6 +31,8 @@ if(!Array.prototype._fromPairs){
 				   length(obj)>=0&&
 				   length(obj)<=Number.MAX_SAFE_INTEGER;
 		};
+
+		// add property.
 		var add=function(array,o){
 			for(var i=0;i<length(array);i+=1){
 				if(!isArrayLike(array[i])){
@@ -19,10 +41,12 @@ if(!Array.prototype._fromPairs){
 				o[array[i][0]]=array[i][1];
 			}
 		};
+
 		return function(){
 			var obj={};
 			add(this,obj);
 			return obj;
 		};
+		
 	})();
 }

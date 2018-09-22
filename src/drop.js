c24af24f6eda8fab7c2fbc2e31b4829ad3556d86
linweiws <1021069119@qq.com>
2018-09-22 09:39:12 +0800

    docs: add comment about drop.js
    Signed-off-by: linweiws <1021069119@qq.com>

--- src/drop.js
+++ src/drop.js
@@ -1,9 +1,33 @@
+/**
+ * 2018-09-22
+ * @LinWei
+ *
+ * feature like _.drop in lodash.
+ * see https://lodash.com/docs/4.17.10#drop.
+ *
+ * for example:
+ *
+ * var array=[1,2,3,4,5];
+ *
+ * console.log(array._drop());
+ * // => [2, 3, 4, 5]
+ *
+ * console.log(array._drop(2));
+ * // => [3, 4, 5]
+ *
+ * console.log(array._drop(100));
+ * // => []
+ *
+ */
+
 if(!Array.prototype._drop){
 	Array.prototype._drop=(function(){
 		"use strict";
+
 		var length=function(obj){
 			return obj.length;
 		};
+
 		var process=function(array,c){
 			switch(true){
 				case c===undefined:
@@ -15,12 +39,14 @@ if(!Array.prototype._drop){
 				     return 0;          
 			}
 		};
+
 		var push=function(array,result,c){
 			var i=c;
 			while(i<length(array)){
 				result.push(array[i++]);
 			}
 		};
+		
 		return function(count){
 			count=process(this,count);
 			var result=[];

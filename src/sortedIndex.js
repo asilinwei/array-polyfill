0d275e3d54cb8bfcf37811a8a21869c14102a736
linweiws <1021069119@qq.com>
2018-09-23 14:33:57 +0800

    docs: add comment about sortedIndex.js
    Signed-off-by: linweiws <1021069119@qq.com>

--- src/sortedIndex.js
+++ src/sortedIndex.js
@@ -1,15 +1,38 @@
+/**
+ * 2018-09-23
+ * @LinWei
+ *
+ * feature like _.sortedIndex in lodash.
+ * see https://lodash.com/docs/4.17.10#sortedIndex.
+ *
+ * for example:
+ * 
+ * var array = [20, 30];
+ *
+ * console.log(array._sortedIndex(25));
+ * // => 1
+ *
+ */
+
 if(!Array.prototype._sortedIndex){
 	Array.prototype._sortedIndex=(function(){
 		"use strict";
+
 		var length=function(array){
 			return array.length;
 		};
+
+		// check if it is string.
 		var isString=function(value){
 			return typeof value==='string';
 		};
+
+		// check if it is number.
 		var isNumber=function(value){
 			return typeof value==='number'&&isFinite(value);
 		};
+
+		// binary search.
 		var find=function(array,value,low,high){
 			if(low<high){
 				var mid=Math.floor((low+high)/2),    
@@ -23,6 +46,7 @@ if(!Array.prototype._sortedIndex){
 			}
 			return high;
 		};
+		
 		return function(value){
 			var low=0,
 				high=length(this);  
